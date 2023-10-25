8554519f07f331372fe30ac48ce48c088cbe5759
Vinicius <violetoru@ymail.com>
2023-10-25 17:50:09 -0300

    Controller Model e Repository prontos !!!

--- src/main/java/com/orusys/crudspringboot/controller/CrudController.java
+++ src/main/java/com/orusys/crudspringboot/controller/CrudController.java
@@ -1,5 +1,124 @@
 package com.orusys.crudspringboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.orusys.crudspringboot.model.Crud;
+import com.orusys.crudspringboot.repository.CrudRepository;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/api")
 public class CrudController {
+
+    @Autowired
+    private CrudRepository crudRepository;
+
+    @GetMapping("/cruds")
+    public ResponseEntity<List<Crud>> getAllCruds(@RequestParam(required = false) String title) {
+        try {
+            List<Crud> cruds = new ArrayList<Crud>();
+
+            if (title == null)
+                crudRepository.findAll().forEach(cruds::add);
+            else
+                crudRepository.findByTitleContaining(title).forEach(cruds::add);
+
+            if (cruds.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(cruds, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/cruds/{id}")
+    public ResponseEntity<Crud> getCrudById(@PathVariable("id") long id) {
+        Optional<Crud> crudData = crudRepository.findById(id);
+
+        if (crudData.isPresent()) {
+            return new ResponseEntity<>(crudData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/cruds")
+    public ResponseEntity<Crud> createCrud(@RequestBody Crud crud) {
+        try {
+            Crud _crud = crudRepository
+                    .save(new Crud(crud.getTitle(), crud.getDescription(), false));
+            return new ResponseEntity<>(_crud, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/cruds/{id}")
+    public ResponseEntity<Crud> updateCrud(@PathVariable("id") long id, @RequestBody Crud crud) {
+        Optional<Crud> crudData = crudRepository.findById(id);
+
+        if (crudData.isPresent()) {
+            Crud _crud = crudData.get();
+            _crud.setTitle(crud.getTitle());
+            _crud.setDescription(crud.getDescription());
+            _crud.setPublished(crud.isPublished());
+            return new ResponseEntity<>(crudRepository.save(_crud), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/cruds/{id}")
+    public ResponseEntity<HttpStatus> deleteCrud(@PathVariable("id") long id) {
+        try {
+            crudRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/cruds")
+    public ResponseEntity<HttpStatus> deleteAllCruds() {
+        try {
+            crudRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("/cruds/published")
+    public ResponseEntity<List<Crud>> findByPublished() {
+        try {
+            List<Crud> cruds = crudRepository.findByPublished(true);
+
+            if (cruds.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(cruds, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
--- src/main/java/com/orusys/crudspringboot/repository/CrudRepository.java
+++ src/main/java/com/orusys/crudspringboot/repository/CrudRepository.java
@@ -2,21 +2,12 @@ package com.orusys.crudspringboot.repository;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import com.orusys.crudspringboot.entity.Crud;
+import com.orusys.crudspringboot.model.Crud;
 
-@Repository
-@Transactional
-public interface CrudRepository extends JpaRepository<Crud, Integer> {
-    List<Crud> findByTitleContainingIgnoreCase(String keyword);
+public interface CrudRepository extends JpaRepository<Crud, Long> {
+    List<Crud> findByPublished(boolean published);
 
-    @Query("UPDATE Tutorial t SET t.published = :published WHERE t.id = :id")
-    @Modifying
-    public void updatePublishedStatus(Integer id, boolean published);
+    List<Crud> findByTitleContaining(String title);
 }
