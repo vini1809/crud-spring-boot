36b72d2f423554f2cae40917a61d5f3fc691ced6
Vinicius <violetoru@ymail.com>
2023-11-02 11:03:52 -0300

    sincr

---
+++ src/main/java/com/orusys/crudspringboot/controller/SobreController.java
@@ -0,0 +1,13 @@
+package com.orusys.crudspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SobreController {
+
+    @GetMapping("/sobre")
+    public String sobre() {
+        return "sobre";
+    }
+}
